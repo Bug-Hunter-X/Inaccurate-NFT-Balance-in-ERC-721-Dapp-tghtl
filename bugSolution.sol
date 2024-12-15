4b0a3904dc7fd85bf6497b091a713d44ea3af339
BugHunter-X <askzenai@gmail.com>
2024-12-15 17:16:06 +0100

    Add bugSolution.sol file

---
+++ bugSolution.sol
@@ -0,0 +1,10 @@
+function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0 ? string(abi.encodePacked(currentBaseURI, tokenId.toString())) : "";
+    } 
